d438fba70902fe8811a3370252077749df170641
brahian <brahian609@hotmail.com>
2016-04-25 11:43:41 -0500

    feat(index): use controller as and dependencies

---
+++ app.controller.js
@@ -0,0 +1,18 @@
+(function () {
+    'user strict';
+
+    angular.module('app').controller('UsersController', UsersController);
+
+    UsersController.$inject = ['$scope', '$timeout'];
+
+    function UsersController($scope, $timeout) {
+
+        this.users = [
+            {name: 'Brahian', last_name: 'Giraldo', money: 20000},
+            {name: 'Pedro', last_name: 'Medina', money: 15000},
+            {name: 'Pablo', last_name: 'Peres', money: 10000}
+        ];
+
+    }
+
+}());
