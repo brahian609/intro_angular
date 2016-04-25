577e66ef3534f39847fa84d6a7814cb1eb27eac1
brahian <brahian609@hotmail.com>
2016-04-25 16:05:57 -0500

    refactor(users.controller): replace function with class ES2015

--- app/components/users/users.controller.js
+++ app/components/users/users.controller.js
@@ -1,18 +1,22 @@
 (function () {
     'user strict';
+    
+    class UsersController {
+        
+        constructor($scope,$timeout){
 
-    angular.module('app.components.users').controller('UsersController', UsersController);
+            this.users = [
+                {name: 'Brahian', last_name: 'Giraldo', money: 20000},
+                {name: 'Pedro', last_name: 'Medina', money: 15000},
+                {name: 'Pablo', last_name: 'Peres', money: 10000}
+            ];
+
+        }
+        
+    }
 
     UsersController.$inject = ['$scope', '$timeout'];
 
-    function UsersController($scope, $timeout) {
-
-        this.users = [
-            {name: 'Brahian', last_name: 'Giraldo', money: 20000},
-            {name: 'Pedro', last_name: 'Medina', money: 15000},
-            {name: 'Pablo', last_name: 'Peres', money: 10000}
-        ];
-
-    }
+    angular.module('app.components.users').controller('UsersController', UsersController);
 
 }());
